8ed3c512044a8c154602dc0135179619552feb02
nastex21 <nasax2000@gmail.com>
2020-05-03 01:10:56 -0500

    Mobile: Hamburger animates

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 var isOpen = false;
 
-  /* Open when someone clicks on the span element */
-function toggleSideBar(){
-  if(!isOpen){
+/* Open when someone clicks on the span element */
+function toggleSideBar() {
+  if (!isOpen) {
     document.getElementById("myNav").style.width = "100%";
     isOpen = true;
   } else {
@@ -11,3 +11,18 @@ function toggleSideBar(){
   }
 }
 
+// Look for .hamburger
+var hamburger = document.querySelector(".hamburger");
+// On click
+hamburger.addEventListener("click", function () {
+  const burgerID = document.getElementById('burgerButton');
+  // Toggle class "is-active"
+  hamburger.classList.toggle("is-active");
+  // Do something else, like open/close menu
+
+  if (burgerID.classList.contains('is-active')){
+    document.getElementById("myNav").style.width = "100%";
+  } else {
+    document.getElementById("myNav").style.width = "0%";
+  }
+});
