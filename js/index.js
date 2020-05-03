f60b69c12a99b29d72bfd3ec28f01582623100be
Tony <nasax2000@gmail.com>
2020-05-02 22:15:25 -0500

    Separated the mobile hamb. menu bar and the nav bar.

--- js/index.js
+++ js/index.js
@@ -1,9 +1,19 @@
-function myFunction() {
+/* function myFunction() {
     var w = window.outerWidth;
     var getID = document.getElementById("nav-bar");
 
-    if (w < 768){
+ /*    if (w < 768){
         console.log(w)
-        getID.className = "sticky-top"
-    }
+        getID.className = "fixedNav"
+    } */
+
+  /* Open when someone clicks on the span element */
+function openNav() {
+    
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  /* Close when someone clicks on the "x" symbol inside the overlay */
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
   }
