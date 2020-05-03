413477c0bbaf77eb916918063d0ce2088d5caf48
nastex21 <nasax2000@gmail.com>
2020-05-02 23:49:36 -0500

    Added a way to toggle mobile navbar button.

--- js/index.js
+++ js/index.js
@@ -1,19 +1,13 @@
-/* function myFunction() {
-    var w = window.outerWidth;
-    var getID = document.getElementById("nav-bar");
-
- /*    if (w < 768){
-        console.log(w)
-        getID.className = "fixedNav"
-    } */
+var isOpen = false;
 
   /* Open when someone clicks on the span element */
-function openNav() {
-    
+function toggleSideBar(){
+  if(!isOpen){
     document.getElementById("myNav").style.width = "100%";
-  }
-  
-  /* Close when someone clicks on the "x" symbol inside the overlay */
-  function closeNav() {
+    isOpen = true;
+  } else {
     document.getElementById("myNav").style.width = "0%";
+    isOpen = false;
   }
+}
+
