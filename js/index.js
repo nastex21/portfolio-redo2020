d01803b059a2a188de64a9f5f3ec23a1d22c8456
Tony <nasax2000@gmail.com>
2020-05-05 22:08:54 -0500

    commit

--- js/index.js
+++ js/index.js
@@ -15,14 +15,49 @@ function toggleSideBar() {
 var hamburger = document.querySelector(".hamburger");
 // On click
 hamburger.addEventListener("click", function () {
-  const burgerID = document.getElementById('burgerButton');
+  const burgerID = document.getElementById("burgerButton");
   // Toggle class "is-active"
   hamburger.classList.toggle("is-active");
   // Do something else, like open/close menu
 
-  if (burgerID.classList.contains('is-active')){
+  if (burgerID.classList.contains("is-active")) {
     document.getElementById("myNav").style.width = "100%";
   } else {
     document.getElementById("myNav").style.width = "0%";
   }
 });
+
+var addNav = '<nav class="navbar navbar-expand-md navbar-dark" style="background-color: #191919;">' +
+'<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation"> ' + 
+'<span class="navbar-toggler-icon"></span>' + 
+'</button>' +
+'<div class="collapse navbar-collapse" id="navbarNav">' +
+'<ul class="navbar-nav">' + 
+'<li class="nav-item active">' +
+'<a class="nav-link" href="/"><span class="navlink-address">Home</span></a>' +
+'</li>' +
+'<li class="nav-item">' +
+'<a class="nav-link" href="/portfolio.html">Portfolio</a>' +
+'</li>' +
+'<li class="nav-item">' +
+'<a class="nav-link" href="/contact.html">Contact</a>' +
+'</li>' +
+'</ul>' +
+'</div>' +
+'</nav>'; 
+
+/* function navBarStart() {
+console.log('started');
+
+}
+
+navBarStart(); */
+
+/* window.onload = (event) => {
+  var start = document.getElementById("nav-bar");
+  var end ="<p>Hi</p>";
+
+  start.insertAdjacentHTML('beforeend', addNav);
+  console.log(start)
+};
+ */
