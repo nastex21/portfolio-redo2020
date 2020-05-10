074fd9035afcbeb2149f7e0ad915605bbf466210
Tony <nasax2000@gmail.com>
2020-05-09 19:58:13 -0500

    Fix portfolio page

--- js/index.js
+++ js/index.js
@@ -26,38 +26,3 @@ hamburger.addEventListener("click", function () {
     document.getElementById("myNav").style.width = "0%";
   }
 });
-
-var addNav = '<nav class="navbar navbar-expand-md navbar-dark" style="background-color: #191919;">' +
-'<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation"> ' + 
-'<span class="navbar-toggler-icon"></span>' + 
-'</button>' +
-'<div class="collapse navbar-collapse" id="navbarNav">' +
-'<ul class="navbar-nav">' + 
-'<li class="nav-item active">' +
-'<a class="nav-link" href="/"><span class="navlink-address">Home</span></a>' +
-'</li>' +
-'<li class="nav-item">' +
-'<a class="nav-link" href="/portfolio.html">Portfolio</a>' +
-'</li>' +
-'<li class="nav-item">' +
-'<a class="nav-link" href="/contact.html">Contact</a>' +
-'</li>' +
-'</ul>' +
-'</div>' +
-'</nav>'; 
-
-/* function navBarStart() {
-console.log('started');
-
-}
-
-navBarStart(); */
-
-/* window.onload = (event) => {
-  var start = document.getElementById("nav-bar");
-  var end ="<p>Hi</p>";
-
-  start.insertAdjacentHTML('beforeend', addNav);
-  console.log(start)
-};
- */
