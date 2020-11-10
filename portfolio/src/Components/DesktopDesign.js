7d35716570b6b31fbd2a0bdb76984d2582a4b75b
nastex21 <nasax2000@gmail.com>
2020-11-10 00:26:27 -0600

    Added Scrollspy

--- portfolio/src/Components/DesktopDesign.js
+++ portfolio/src/Components/DesktopDesign.js
@@ -3,8 +3,6 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Home from "./DesktopDesign/Home";
 import NavBarItems from "./DesktopDesign/NavBarItems";
-import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
 import Services from "./DesktopDesign/Services";
 import Skills from "./DesktopDesign/Skills";
 import Portfolio from "./DesktopDesign/Portfolio";
@@ -12,81 +10,7 @@ import Contact from "./DesktopDesign/Contact";
 import Footer from "./DesktopDesign/Footer";
 import "./css/Desktop.css";
 
-const getDimensions = (ele) => {
-  if (!ele) return;
-  console.log("ele");
-  console.log(ele);
-  console.log(ele.getBoundingClientRect());
-
-
-  const offsetWidth = ele.offsetWidth;
-
-  console.log(offsetWidth);
-
-  return {
-
-  };
-};
-
 function DesktopDesign() {
-  const [visibleSection, setVisibleSection] = useState();
-  const headerRef = useRef(null);
-  const homeRef = useRef(null);
-  const skillsRef = useRef(null);
-  const servicesRef = useRef(null);
-  const portfolioRef = useRef(null);
-  const contactRef = useRef(null);
-  const sectionRefs = [
-    { section: "home", ref: homeRef },
-    { section: "skills", ref: skillsRef },
-    { section: "services", ref: servicesRef },
-    { section: "portfolio", ref: portfolioRef },
-    { section: "contact", ref: contactRef },
-  ];
-
-  //scrolls to address in nav
-  const scrollTo = (ele) => {
-    console.log(ele);
-    if(!ele) return;
-    ele.scrollIntoView({
-      behavior: "smooth",
-    });
-  };
-
-  useEffect(() => {
-   /*  const handleScroll = (e) => {
-      console.log(e);
-      if (!headerRef.current) return;
-      const { height: headerHeight } = getDimensions(headerRef.current);
-      const scrollPosition = window.scrollX;
-      console.log("hi2");
-      console.log(scrollPosition);
-
-      const selected = sectionRefs.find(({ section, ref }) => {
-        console.log(ref);
-        console.log(section);
-        const ele = ref.current;
-        console.log(ele);
-        if (ele) {
-          const { offsetBottom, offsetTop } = getDimensions(ele);
-          return scrollPosition > offsetTop && scrollPosition < offsetBottom;
-        }
-      });
-
-      if (selected && selected.section !== visibleSection) {
-        setVisibleSection(selected.section);
-      } else if (!selected && visibleSection) {
-        setVisibleSection(undefined);
-      }
-    }; */
-
-    handleScroll();
-    window.addEventListener("keydown", handleScroll);
-    return () => {
-      window.removeEventListener("keydown", handleScroll);
-    };
-  }, [visibleSection]);
-
 
   return (
     <>
@@ -98,78 +22,15 @@ function DesktopDesign() {
             </picture>
           </div>
           <div className="right-container">
-            <div className="navbarDiv mb-5" id="navbarIndex" ref={headerRef}>
-              <Navbar fixed="top">
-                <Nav>
-                  <Nav.Item>
-                    <Nav.Link
-                      href="/home"
-                      className={`header_link ${
-                        visibleSection === "Home" ? "selected" : ""
-                      }`}
-                      onClick={() => {
-                        scrollTo(homeRef.current);
-                      }}>
-                      Home
-                    </Nav.Link>
-                  </Nav.Item>
-                  <Nav.Item>
-                    <Nav.Link
-                      href="#services"
-                      className={`header_link ${
-                        visibleSection === "Services" ? "selected" : ""
-                      }`}
-                      onClick={() => {
-                        scrollTo(servicesRef.current)
-                      }}>
-                      Services
-                    </Nav.Link>
-                  </Nav.Item>
-                  <Nav.Item>
-                    <Nav.Link
-                      href="#skills"
-                      className={`header_link ${
-                        visibleSection === "Skills" ? "selected" : ""
-                      }`}
-                      onClick={() => {
-                        scrollTo(skillsRef.current) 
-                      }}>
-                      Skills
-                    </Nav.Link>
-                  </Nav.Item>
-                  <Nav.Item>
-                    <Nav.Link
-                      href="#portfolio"
-                      className={`header_link ${
-                        visibleSection === "Portfolio" ? "selected" : ""
-                      }`}
-                      onClick={() => {
-                        scrollTo(portfolioRef.current);
-                      }}>
-                      Portfolio
-                    </Nav.Link>
-                  </Nav.Item>
-                  <Nav.Item>
-                    <Nav.Link
-                      href="#contact"
-                      className={`header_link ${
-                        visibleSection === "Contact" ? "selected" : ""
-                      }`}
-                      onClick={() => {
-                        scrollTo(contactRef.current);
-                      }}>
-                      Contact
-                    </Nav.Link>
-                  </Nav.Item>
-                </Nav>
-              </Navbar>
+            <div className="navbarDiv mb-5" id="navbarIndex">
+              <NavBarItems />
             </div>
             <div className="contentDiv">
-              <Home ref={homeRef} />
-              <Services ref={servicesRef} />      
-              <Skills ref={skillsRef} />
-              <Portfolio ref={portfolioRef} />
-              <Contact ref={contactRef} />
+              <Home />
+              <Services />      
+              <Skills />
+              <Portfolio />
+              <Contact />
             </div>
           </div>
         </Row>
--- portfolio/src/Components/DesktopDesign/NavBarItems.js
+++ portfolio/src/Components/DesktopDesign/NavBarItems.js
@@ -1,42 +1,36 @@
 import React, { useEffect, useRef, useState } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
+import Scrollspy from 'react-scrollspy-highlight';
 
 function NavBarItems() {
-  const scrollTo = ele => {
-    console.log('called');
-    ele.scrollIntoView({
-      behavior: "smooth",
-      block: "start",
-    });
-  };
-  const homeRef = useRef(null);
-  const skillsRef = useRef(null);
-  const servicesRef = useRef(null);
-  const portfolioRef = useRef(null);
-  const contactRef = useRef(null);
+
   return (
-    <div className="navbarDiv mb-5" id="navbarIndex">
-      <Navbar fixed="top">
-        <Nav>
-          <Nav.Item>
-            <Nav.Link href="/home" onClick={() => {scrollTo(homeRef.current)}}>Home</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link href="#services" onClick={() => {scrollTo(servicesRef.current)}}>Services</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link href="#skills" onClick={() => {scrollTo(skillsRef.current)}}>Skills</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link href="#portfolio" onClick={() => {scrollTo(portfolioRef.current)}}>Portfolio</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link href="#contact" onClick={() => {scrollTo(contactRef.current)}}>Contact</Nav.Link>
-          </Nav.Item>
-        </Nav>
-      </Navbar>
-    </div>
+    <nav className="nav">
+      <div className="nav__item nav__item--inverse">
+        <a href="/" className="nav__link nav__title">React Scrollspy</a>
+      </div>
+      <Scrollspy
+        items={['Home', 'Services', 'Skills', 'Portfolio', 'Contact']}
+        currentClassName="nav__item--active"
+        className="nav__inner"
+        style={{
+          fontWeight: 300
+        }}
+        offset={-50}
+        onUpdate={
+          (el) => {
+            console.log(el)
+          }
+        }
+      >
+        <li className="nav__item"><a href="#Home" className="nav__link">Home</a></li>
+        <li className="nav__item"><a href="#Services" className="nav__link">Services</a></li>
+        <li className="nav__item"><a href="#Skills" className="nav__link">Skills</a></li>
+        <li className="nav__item"><a href="#Portfolio" className="nav__link">Portfolio</a></li>
+        <li className="nav__item"><a href="#Contact" className="nav__link">Contact</a></li>
+      </Scrollspy>
+    </nav>
   );
 }
 
