409a6beadf488ceee385cf9fa00b175a2a70c06a
nastex21 <nasax2000@gmail.com>
2021-02-25 16:09:10 -0600

    Added aboutme component, css and to navbar

--- portfolio/src/Components/DesktopDesign/AboutMe.js
+++ portfolio/src/Components/DesktopDesign/AboutMe.js
@@ -3,7 +3,7 @@ import React from 'react';
 function AboutMe () {
 
     return (
-        <section id="portfolio" className="contentDiv">
+        <section id="aboutme" className="contentDiv">
             <p>About Me</p>
         </section>
     )
--- portfolio/src/Components/DesktopDesign/NavBarItems.js
+++ portfolio/src/Components/DesktopDesign/NavBarItems.js
@@ -8,19 +8,22 @@ function NavBarItems({ activeClass }) {
       <Navbar id="navbar">
         <Nav>
           <Nav.Item>
-            <Nav.Link className={activeClass == 'home' ? 'is-active' : null} href="#homeSec" data-scroll="#home">Home</Nav.Link>
+            <Nav.Link className={activeClass == 'home' ? 'is-active' : null} href="#home" data-scroll="#home">Home</Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link className={activeClass == 'services' ? 'is-active' : null} href="#servicesSec"  data-scroll="#services">Services</Nav.Link>
+            <Nav.Link className={activeClass == 'services' ? 'is-active' : null} href="#services"  data-scroll="#services">Services</Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link className={activeClass == 'skills' ? 'is-active' : null} href="#skillsSec" data-scroll="#skills">Skills</Nav.Link>
+            <Nav.Link className={activeClass == 'skills' ? 'is-active' : null} href="#skills" data-scroll="#skills">Skills</Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link className={activeClass == 'portfolio' ? 'is-active' : null} href="#portfolioSec" data-scroll="#portfolio">Portfolio</Nav.Link>
+            <Nav.Link className={activeClass == 'portfolio' ? 'is-active' : null} href="#portfolio" data-scroll="#portfolio">Portfolio</Nav.Link>
+          </Nav.Item>
+                 <Nav.Item>
+            <Nav.Link className={activeClass == 'aboutme' ? 'is-active' : null} href="#aboutme" data-scroll="#aboutme">About Me</Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link className={activeClass == 'contact' ? 'is-active' : null} href="#contactSec" data-scroll="#contact">Contact</Nav.Link>
+            <Nav.Link className={activeClass == 'contact' ? 'is-active' : null} href="#contact" data-scroll="#contact">Contact</Nav.Link>
           </Nav.Item>
         </Nav>
       </Navbar>
