39c2d408fc19e281546c511a5f108659590f2763
Tony <nasax2000@gmail.com>
2021-02-25 21:23:53 -0600

    Added router and put most logic in App.js and remove DesktopDesign.js

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,15 +1,62 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import useWindowSize from './util/WindowSize';
+import SideBar from "./Components/DesktopDesign/Component/SideBar";
+import Home from './Components/DesktopDesign/Home';
+import AboutMe from './Components/DesktopDesign/AboutMe';
+import Skills from './Components/DesktopDesign/Skills';
+import Services from './Components/DesktopDesign/Services';
+import Portfolio from './Components/DesktopDesign/Portfolio';
+import Contact from './Components/DesktopDesign/Contact';
 import MobileDesign from './Components/MobileDesign';
-import DesktopDesign from './Components/DesktopDesign';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   const size = useWindowSize();
 
+  const desktopFunc = () => {
+    return (
+    <Router>
+    <div>
+      <SideBar />
+
+      <Switch>
+        <Route path="/aboutme">
+          <AboutMe />
+        </Route>
+        <Route path="/skills">
+          <Skills />
+        </Route>
+        <Route path="/services">
+          <Services />
+        </Route>
+        <Route path="/portfolio">
+          <Portfolio />
+        </Route>
+        <Route path="/Contact">
+          <Contact />
+        </Route>
+        <Route path="/sklls">
+          <Skills />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </div>
+  </Router>
+    )
+  }
+
   return (
     <div className="App">
-      {size.width < 768 ? <MobileDesign /> : <DesktopDesign />} 
+      { size.width < 768 ? <MobileDesign /> : desktopFunc() }
+        } 
     </div>
   );
 }
--- portfolio/src/Components/DesktopDesign/Component/SideBar.js
+++ portfolio/src/Components/DesktopDesign/Component/SideBar.js
@@ -1,13 +1,48 @@
-import React from 'react';
+import React from "react";
 
-function SideBar () {
-
-
-    return (
-        <div>
-            
+function SideBar() {
+  return (
+    <div className="wrapper">
+      <nav id="sidebar">
+        <div className="sidebar-header">
+          <h3>Bootstrap Sidebar</h3>
         </div>
-    )
+
+        <ul className="list-unstyled components">
+          <p>Dummy Heading</p>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/services">Services</a>
+          </li>
+          <li>
+            <a href="/skills">Skills</a>
+          </li>
+          <li>
+            <a href="/portfolio">Portfolio</a>
+          </li>
+          <li>
+              <a href="/aboutme">About Me</a>
+          </li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
+
+      <div id="content">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <div className="container-fluid">
+            <button type="button" id="sidebarCollapse" className="btn btn-info">
+              <i className="fas fa-align-left"></i>
+              <span>Toggle Sidebar</span>
+            </button>
+          </div>
+        </nav>
+      </div>
+    </div>
+  );
 }
 
 export default SideBar;
