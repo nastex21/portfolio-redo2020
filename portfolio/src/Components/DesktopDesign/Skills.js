45de4587e4b629934faca6ea2415a0365ee00573
nastex21 <nasax2000@gmail.com>
2021-02-28 09:51:00 -0600

    Basic foundation for skills done

--- portfolio/src/Components/DesktopDesign/Skills.js
+++ portfolio/src/Components/DesktopDesign/Skills.js
@@ -1,4 +1,6 @@
 import React, { useEffect } from 'react';
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
 import Footer from './Footer';
 
 function Skills({ setPath }) {
@@ -9,31 +11,53 @@ function Skills({ setPath }) {
     <div className="contentDiv">
       <section id="skills">
         <h1>What I Know</h1>
-        <p>Front End</p>
-        <ul>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>JavaScript</li>
-          <li>React</li>
-          <li>Bootstrap</li>
-          <li>Mobile responsiveness</li>
-          <li>github</li>
-        </ul>
-        <p>Back-end</p>
-        <ul>
-          <li>node.js</li>
-          <li>mongodb</li>
-          <li>Linux</li>
-        </ul>
-        <p>Have some knowledge in:</p>
-        <ul>
-          <li>Adobe Photoshop</li>
-          <li>Adobe Illustrator</li>
-          <li>Adobe XD</li>
-          <li>d3.js</li>
-          <li>redux</li>
-          <li>python</li>
-        </ul>
+        <div id="skillsCards">
+          <Card>
+            <Card.Header as="h1">Front-end</Card.Header>
+            <Card.Body>
+              <Card.Text>
+                <ListGroup variant="flush">
+                  <ListGroup.Item>HTML</ListGroup.Item>
+                  <ListGroup.Item>CSS</ListGroup.Item>
+                  <ListGroup.Item>JavaScript</ListGroup.Item>
+                  <ListGroup.Item>React</ListGroup.Item>
+                  <ListGroup.Item>Redux</ListGroup.Item>
+                  <ListGroup.Item>Bootstrap</ListGroup.Item>
+                  <ListGroup.Item>Mobile responsiveness</ListGroup.Item>
+                  <ListGroup.Item>Github</ListGroup.Item>
+                </ListGroup>
+              </Card.Text>
+            </Card.Body>
+          </Card>
+
+          <Card>
+            <Card.Header as="h1">Back-end</Card.Header>
+            <Card.Body>
+              <Card.Text>
+                <ListGroup variant="flush">
+                  <ListGroup.Item>node.js</ListGroup.Item>
+                  <ListGroup.Item>mongodb</ListGroup.Item>
+                  <ListGroup.Item>Linux</ListGroup.Item>
+                </ListGroup>
+              </Card.Text>
+            </Card.Body>
+          </Card>
+
+          <Card>
+            <Card.Header as="h1">Have some knowledge in</Card.Header>
+            <Card.Body>
+              <Card.Text>
+                <ListGroup variant="flush">
+                  <ListGroup.Item>Adobe Photoshop</ListGroup.Item>
+                  <ListGroup.Item>Adobe Illustrator</ListGroup.Item>
+                  <ListGroup.Item>Adobe XD</ListGroup.Item>
+                  <ListGroup.Item>Python</ListGroup.Item>
+                  <ListGroup.Item>PHP</ListGroup.Item>
+                </ListGroup>
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </div>
         <p>
           Create functional websites and apps using HTML, CSS, and JavaScript Have
           knowledge of JavaScript libraries like React & Redux, d3.js Use CSS
@@ -43,7 +67,7 @@ function Skills({ setPath }) {
           Hub Worked with APIs such as Google Maps, Wikipedia, Twitch,
           OpenWeatherMap, etc Experienced with macOS and Linux, and their
           terminals Self-motivated individual that’s not afraid of learning new
-        programming languages or frameworks.{" "}
+          programming languages or frameworks.
         </p>
       </section>
       <Footer />
