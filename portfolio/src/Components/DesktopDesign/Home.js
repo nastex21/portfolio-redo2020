f5e294cb1675e21e889bcdc1435d0c33e0961b6e
nastex21 <nasax2000@gmail.com>
2021-03-14 23:49:29 -0500

    Added font Selawik, content, arrow scroller, stylings.
    Just need my logo on top.

--- portfolio/src/Components/DesktopDesign/Home.js
+++ portfolio/src/Components/DesktopDesign/Home.js
@@ -2,9 +2,26 @@ import React, { useEffect } from "react";
 
 function Home() {
   return (
-      <div id="splashPage">
- 
+    <div id="splashPage">
+      <div id="leftside">
+        <p>I'm</p>
       </div>
+      <div id="rightside">
+        <div id="rightText">
+          <p>a <span className="emphasisWord">DREAMER</span>.</p>
+          <p>a <span className="emphasisWord">BUILDER</span>.</p>
+          <p>a <span className="emphasisWord">CREATOR</span>.</p>
+          <p>a <span className="emphasisWord">MAINTAINER</span>.</p>
+          <p>a <span className="emphasisWord">DEVELOPER</span>.</p>
+          <p className="myName">TONY<span id="lastName">SALAZAR</span>.</p>
+        </div>
+        <a href="#aboutme" class="arrow-container">
+          <div class="arrow"></div>
+          <div class="arrow"></div>
+          <div class="arrow"></div>
+        </a>
+      </div>
+    </div>
   );
 }
 
