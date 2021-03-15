38c5b6d0b16e681b48e986bd6616d8995cfec6f2
Tony <nasax2000@gmail.com>
2021-03-15 02:14:59 -0500

    Added personal brand logo

--- portfolio/src/Components/DesktopDesign/Home.js
+++ portfolio/src/Components/DesktopDesign/Home.js
@@ -1,9 +1,11 @@
 import React, { useEffect } from "react";
+import logo from '../../images/personallogo.jpg';
 
 function Home() {
   return (
     <div id="splashPage">
       <div id="leftside">
+        <img id="logo" src={logo} alt="personal logo with T and S"/>
         <p>I'm</p>
       </div>
       <div id="rightside">
@@ -15,10 +17,10 @@ function Home() {
           <p>a <span className="emphasisWord">DEVELOPER</span>.</p>
           <p className="myName">TONY<span id="lastName">SALAZAR</span>.</p>
         </div>
-        <a href="#aboutme" class="arrow-container">
-          <div class="arrow"></div>
-          <div class="arrow"></div>
-          <div class="arrow"></div>
+        <a href="#aboutme" className="arrow-container">
+          <div className="arrow"></div>
+          <div className="arrow"></div>
+          <div className="arrow"></div>
         </a>
       </div>
     </div>
