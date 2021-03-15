b08f8fe2b5e444a3b835082eeb956c11470e4284
nastex21 <nasax2000@gmail.com>
2021-03-15 18:54:57 -0500

    Filled in content for about me

--- portfolio/src/Components/DesktopDesign/AboutMe.js
+++ portfolio/src/Components/DesktopDesign/AboutMe.js
@@ -10,28 +10,31 @@ function AboutMe() {
             <div id="middleContent">
                 <div id="meContent">
                     <p>
-                        Hello, my name is <strong className="orange">Tony</strong> and I’m a <strong className="orange">passionate software engineer</strong>! I love taking what are originally thoughts and ideas, putting pen to paper
-          and then creating fully-fledged web applications! And with new
-          technologies on the horizon and the field always evolving, part of the
-          thrill is to be on your toes to learn new things. So thank you for
-          taking your time visiting my page and I hope you learn more about me
-          and the things I can do!{" "}
+                        Hello, my name is <strong className="orange">Tony</strong> and I’m a <strong className="orange">passionate software engineer</strong>! I love taking what are originally thoughts and ideas, putting pen to paper and then creating fully-fledged web applications! And with new technologies on the horizon and the field always evolving, part of the thrill is to be on your toes to learn new things. So thank you for taking your time visiting my page and I hope you learn more about me and the things I can do!
                     </p>
                 </div>
                 <div id="meStats">
-                    <p>kalsdjfa test</p>
-                    <p>sldkjf</p>
+                    <p>Name: Tony Salazar</p>
+                    <p>email: <a href="tonysal@tonysal.met">tonysal@tonysal.net</a></p>
+                    <p>Age: 38</p>
+                    <p>From: San Antonio, Texas</p>
+                    <button>Download Resume</button>
                 </div>
             </div>
             <div id="bottomContent">
                 <div id="education">
+                    <h3>Eduation</h3>
                     <p>St. Mary's University</p>
+                    <p>FreeCodeCamp</p>
+                    <p>St. Philip's College</p>
                 </div>
                 <div id="experience">
-                    <p>Years of Experience</p>
+                    <h3>Years of Experience</h3>
+                    <p>4</p>
                 </div>
                 <div id="projectsNum">
-                    <p>Projects</p>
+                    <h3>Projects</h3>
+                    <p>30+</p>
                 </div>
             </div>
         </section>
