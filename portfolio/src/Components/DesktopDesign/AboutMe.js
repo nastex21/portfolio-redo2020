cc8a9455b054b6679c01ca8fa8c1fec176c567bb
nastex21 <nasax2000@gmail.com>
2021-03-16 00:12:34 -0500

    Added large screen media query

--- portfolio/src/Components/DesktopDesign/AboutMe.js
+++ portfolio/src/Components/DesktopDesign/AboutMe.js
@@ -8,12 +8,12 @@ function AboutMe() {
                 <h1>Who am I?</h1>
             </div>
             <div id="middleContent">
-                <div id="meContent">
+                <div id="contentFont meContent">
                     <p>
                         Hello, my name is <strong className="orange">Tony</strong> and I’m a <strong className="orange">passionate software engineer</strong>! I love taking what are originally thoughts and ideas, putting pen to paper and then creating fully-fledged web applications! And with new technologies on the horizon and the field always evolving, part of the thrill is to be on your toes to learn new things. So thank you for taking your time visiting my page and I hope you learn more about me and the things I can do!
                     </p>
                 </div>
-                <div id="meStats">
+                <div id="meStats" className="contentFont">
                     <p>Name: Tony Salazar</p>
                     <p>email: <a href="tonysal@tonysal.met">tonysal@tonysal.net</a></p>
                     <p>Age: 38</p>
@@ -22,17 +22,17 @@ function AboutMe() {
                 </div>
             </div>
             <div id="bottomContent">
-                <div id="education">
+                <div id="education" className="contentFont">
                     <h3>Eduation</h3>
                     <p>St. Mary's University</p>
                     <p>FreeCodeCamp</p>
                     <p>St. Philip's College</p>
                 </div>
-                <div id="experience">
+                <div id="experience" className="contentFont">
                     <h3>Years of Experience</h3>
                     <p>4</p>
                 </div>
-                <div id="projectsNum">
+                <div id="projectsNum" className="contentFont">
                     <h1>Projects</h1>
                     <p>30+</p>
                 </div>
